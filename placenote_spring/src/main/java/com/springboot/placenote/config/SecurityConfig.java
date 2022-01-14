39d4f056acedd419819265273510c4305f2cc57f
kzzzzmi <jaemin21685@gmail.com>
2022-01-14 18:44:14 +0900

    html, spring 작업 공간 분리

--- placenote_spring/src/main/java/com/springboot/placenote/config/SecurityConfig.java
+++ placenote_spring/src/main/java/com/springboot/placenote/config/SecurityConfig.java
@@ -1,14 +1,34 @@
 package com.springboot.placenote.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		
+		http.authorizeRequests()
+			.antMatchers("/", "/index")
+			.authenticated()
+			.anyRequest()
+			.permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/auth/signin")
+
+			.defaultSuccessUrl("/index");
 	}
-	
 }
---
+++ placenote_spring/src/main/java/com/springboot/placenote/web/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.springboot.placenote.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+	
+	@PostMapping("/auth/signin")
+	public void auth() {
+		
+	}
+	
+}
