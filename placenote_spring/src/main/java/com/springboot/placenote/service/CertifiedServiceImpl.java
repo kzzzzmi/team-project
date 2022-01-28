e518ecde4871e74baaec63fa0943f0d9a2aadc61
ydg90 <ydg9002@naver.com>
2022-01-28 17:42:29 +0900

    휴대폰 인증 변경

--- placenote_spring/src/main/java/com/springboot/placenote/service/CertifiedServiceImpl.java
+++ placenote_spring/src/main/java/com/springboot/placenote/service/CertifiedServiceImpl.java
@@ -13,13 +13,13 @@ public class CertifiedServiceImpl implements CertifiedService {
 
 	@Override
 	public void certifiedPhoneNumber(String phoneNumber, String cerNum) {
-		String api_key = "NCSPTKRF0KTVR9NT";
-		String api_secret = "TTS4KZTRWJ9R9O7IIDB98ZWD6KRKJ1UJ";
+		String api_key = "NCSX3WS9ZKISQROY";
+		String api_secret = "IDT1FNJMUDQOM2MIUQE8H7FY9NZQFJTM";
 		Message coolsms = new Message(api_key, api_secret);
 		
 		HashMap<String, String> params = new HashMap<>();
 		params.put("to", phoneNumber);
-		params.put("from", "010-7653-1685");
+		params.put("from", "010-7700-4043");
 		params.put("type", "SMS");
 		params.put("text", "휴대폰 인증 메세지 : 인증번호는 [" + cerNum + "] 입니다.");
 		params.put("app_version", "test app 1.2");
